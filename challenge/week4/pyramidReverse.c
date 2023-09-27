ac24bbaff71cc0ff5000b090edeabe81216488e8
cksals0316 <cksals7656@naver.com>
2023-09-27 15:57:39 +0900

    Week4.2. 역피라미드

--- challenge/week4/pyramidReverse.c
+++ challenge/week4/pyramidReverse.c
@@ -3,17 +3,17 @@
 int main()
 {
 	int floor;
-	printf("몇 층을 쌓겠습니까? (5~100)");
+	printf("몇 층을 쌓겠습니까? (5~100)");  // 사용자에게 입력값
 	scanf_s("%d", &floor);
 
-	for (int i = floor; i > 0; i--) {
-		for (int j = 0; j < floor - i; j++) {
+	for (int i = floor; i > 0; i--) {  // i 가 0보다 크다는 조건이 성립했을때 i는 1씩 감량
+		for (int j = 0; j < floor - i; j++) {  // fllor - i 만큼 커진다
 			printf("S");
 		}
-		for (int k = 0; k < i * 2 - 1; k++) {
+		for (int k = 0; k < i * 2 - 1; k++) {  // i*2-1 만큼 커진다.
 			printf("*");
 		}
-		printf("\n");
+		printf("\n");  // 줄바꿈
 
 	}
 	return 0;
