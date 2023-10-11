30ed69d2471a443a635ffabf34682eb1a7162e70
cksals0316 <cksals7656@naver.com>
2023-10-11 16:58:16 +0900

    Week6. 배열

---
+++ challenge/wekk6/studentGrade.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#define STUDENTS 5  // 학생들의 수 정의
+
+void classifyStudents(int scores[], char targetGrade) {  // 함수 정의
+	printf("학생 성적 분류:\n");
+	char grade = ' ';
+	for (int i = 0; i < STUDENTS; i++) {  // 일정 점수에 따라 성적이 달라지는 조건문
+		if (scores[i] >= 90) {
+			grade = 'A';
+		}
+		else if (scores[i] >= 80) {
+			grade = 'B';
+		}
+		else if (scores[i] >= 70) {
+			grade = 'C';
+		}
+		else if (scores[i] >= 60) {
+			grade = 'D';
+		}
+		else {
+			grade = 'F';
+		}
+		if (targetGrade == grade) {
+			printf("%d 학생은 %c 점수를 받았습니다.", i + 1, targetGrade);
+		}
+	}
+}
+
+int sumScores(int scores[]) {  // 함수 선언
+	int sum = 0;
+
+	for (int i = 0; i < STUDENTS; i++) {
+		sum += scores[i];  // 학생들의 점수를 전체 더하기
+	}
+}
+
+double averageScores(int scores[]) {
+	double average;
+	double sum = 0;
+	for (int i = 0; i < STUDENTS; i++) {
+		sum += scores[i];  //점수 평균 구하기
+	}
+	average = sum / STUDENTS;
+}
+
+
+void printRanks(int scores[]) {
+	int scores[STUDENTS];
+}
+
+
+
+int main(){
+	int scores[STUDENTS];  //성적을 저장할 배열 선언
+
+	for (int i = 0; i < STUDENTS; i++) {
+		printf("학생 %d의 성적을 입력하세요: ", i + 1);  //학생들의 성적 입력받기
+		scanf_s("%d", &scores[i]);
+	}
+
+	char ch = getchar();
+
+	char target;
+	printf("특정 점수 (A, B, C, D, F)를 입력하시오");
+	scanf_s("%c", &target, 1);
+	
+	classifyStudents(scores, target);  //학생들의 성적 분류 및 출력
+
+	int sum = sumScores(scores);
+	double average = averageScores(scores);
+	printf("학생들 점수의 총 합은 %d 이고, 평균 값은 %lf입니다 \n", sum, average)
+
+	printRanks(scores);
+
+	return 0;
+}
